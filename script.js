790f951357ccd29da7e880389d9fb69186a02eac
Stoney <higboko@gmail.com>
2024-08-08 12:51:46 +0100

    download cv

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// About Me 
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -10,4 +11,16 @@ function opentab(tabname) {
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Download CV
+function downloadCV() {
+    const fileId = '1jViCz9sZAzlyKQwsR5Q97Q6ayfeBBo412dM6qz4-Ank';
+    const fileUrl = `https://docs.google.com/document/d/${fileId}/export?format=pdf`;
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'CV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
